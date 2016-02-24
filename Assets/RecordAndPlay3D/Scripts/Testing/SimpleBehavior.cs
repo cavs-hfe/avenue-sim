068aa25a03f53f79b6be94357ed4d0a0c7624bcb
Eli Davis <eli.davis1995@gmail.com>
2016-02-23 22:53:37 -0600

    Re imported Record features as package
    Deleted what there was of the recording features and imported the
    segmented project as it's own package into this project all contents
    stored within a folder labeld "RecordAndPlay3D"

---
+++ Assets/RecordAndPlay3D/Scripts/Testing/SimpleBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CAVS.Recording.Testing {
+
+	public class SimpleBehavior : CAVS.Recording.PlaybackActorBehavior {
+
+
+		public override void handleEvent(string name, string contents){
+			Debug.Log (name + " : " + contents);
+		}
+
+
+		// Update is called once per frame
+		void Update () {
+			transform.Translate (Mathf.Cos(Time.time) * 1 *Time.deltaTime, Mathf.Sin(Time.time) * 1 *Time.deltaTime, 0);
+			transform.Rotate (new Vector3(0,10*Time.deltaTime,0));
+
+			if (Input.GetKeyDown (KeyCode.A)) {
+				RecordingServiceBehavior service = GameObject.FindObjectOfType<RecordingServiceBehavior> ();
+				service.logEventToRecording ("Input","A");
+				Debug.Log ("Hit A");
+			}
+
+		}
+
+
+	}
+
+}
