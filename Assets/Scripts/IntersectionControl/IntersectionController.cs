7578168ed5ddb704c28117e22fad793199c13290
Eli Davis <eli.davis1995@gmail.com>
2016-03-01 22:16:30 -0600

    Intersection Controller Added
    A traffic light prefab has been created which fixes some rotation
    problems of the original asset as well has the traffic light behavior
    attatched to it which eases control over the traffic light.  Demo Scene
    has been added to showcase functionality.

---
+++ Assets/Scripts/IntersectionControl/IntersectionController.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CAVS.IntersectionControl{
+
+	public class IntersectionController : MonoBehaviour {
+
+		[SerializeField]
+		private List<StopLightBehavior> northSouthStopLights;
+
+
+		[SerializeField]
+		private List<StopLightBehavior> eastWestStopLights;
+
+
+		/// <summary>
+		/// Are the south/north lanes currentely green
+		/// </summary>
+		private bool snEnabled = true;
+
+
+		/// <summary>
+		/// Adds the stop light to the north/south category.
+		/// </summary>
+		/// <param name="stoplight">Stoplight.</param>
+		public void addNorthSouthStopLight(StopLightBehavior stoplight){
+			this.northSouthStopLights.Add (stoplight);
+		}
+
+
+		/// <summary>
+		/// Adds the stop light to the east/west category.
+		/// </summary>
+		/// <param name="stoplight">Stoplight.</param>
+		public void addEastWestStopLight(StopLightBehavior stoplight){
+			this.eastWestStopLights.Add (stoplight);
+		}
+
+
+		/// <summary>
+		/// Toggles what road is currentely green.
+		/// </summary>
+		public void toggle(){
+
+			toggle (0);
+
+		} 
+
+
+		/// <summary>
+		/// Toggle the specified delay.
+		/// </summary>
+		/// <param name="delay">Delay.</param>
+		public void toggle(float delay){
+
+			toggle (delay, 0);
+
+		}
+
+
+		/// <summary>
+		/// Toggles which of the lanes are green.
+		/// </summary>
+		/// <param name="yellowLightDuration">how long the currentely green lane will stay in the yellow light before turning red.</param>
+		/// <param name="greenLightduractionAfterRed">How long the originally red light will stay red after the green lane turning red.</param>
+		public void toggle(float yellowLightDuration, float greenLightduractionAfterRed){
+
+			if (snEnabled) {
+				switchLightsToStop (northSouthStopLights.ToArray(), yellowLightDuration);
+				switchLightsToGo (eastWestStopLights.ToArray(), yellowLightDuration+greenLightduractionAfterRed);
+			} else {
+				switchLightsToStop (eastWestStopLights.ToArray(), yellowLightDuration);
+				switchLightsToGo (northSouthStopLights.ToArray(), yellowLightDuration+greenLightduractionAfterRed);
+			}
+
+			snEnabled = !snEnabled;
+
+		}
+
+
+
+		private void switchLightsToStop(StopLightBehavior[] lights, float delay){
+
+			float sanatizedDelay = Mathf.Clamp (delay, 0, 100000);
+
+			for (int i = 0; i < lights.Length; i++) {
+
+				if (delay != 0) {
+
+					lights [i].changeToYellow ();
+					lights [i].changeToRed (sanatizedDelay);
+
+				} else {
+
+					lights [i].changeToRed ();
+
+				}
+
+			}
+
+		}
+
+
+		private void switchLightsToGo(StopLightBehavior[] lights, float delay){
+
+			float sanatizedDelay = Mathf.Clamp (delay, 0, 100000);
+
+			for (int i = 0; i < lights.Length; i++) {
+
+				if (delay != 0) {
+
+					lights [i].changeToGreen (delay);
+
+				} else {
+
+					lights [i].changeToGreen ();
+
+				}
+
+			}
+
+		}
+
+
+		private float lastDemoToggleTime = -1;
+
+		/// <summary>
+		/// Toggles lights after a short amount of time
+		/// </summary>
+		private void demo(){
+
+			if(lastDemoToggleTime  +6f < Time.time){
+
+				lastDemoToggleTime = Time.time;
+
+				toggle (2, .5f);
+
+			}
+
+		}
+
+
+		void Start(){
+
+			if (northSouthStopLights == null) {
+				northSouthStopLights = new List<StopLightBehavior> ();
+			}
+
+			if (eastWestStopLights == null) {
+				eastWestStopLights = new List<StopLightBehavior> (); 
+			}
+
+			switchLightsToGo (northSouthStopLights.ToArray(), 0);
+			switchLightsToStop (eastWestStopLights.ToArray(), 0);
+			snEnabled = true;
+
+		}
+
+
+
+		// Update is called once per frame
+		void Update () {
+		
+			demo ();
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/IntersectionControl/StopLightBehavior.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CAVS.IntersectionControl {
+
+	public class StopLightBehavior : MonoBehaviour {
+
+		List<float> queuedLightTimes;
+		List<int> queuedLightTypes;
+
+		void Awake(){
+			queuedLightTimes = new List<float> ();
+			queuedLightTypes = new List<int> ();
+		}
+			
+
+		// Update is called once per frame
+		void Update () {
+
+			for (int i = 0; i < queuedLightTimes.Count; i++) {
+
+				if(queuedLightTimes[i] <= Time.time){
+
+					switch(queuedLightTypes[i]){
+
+					case 0:
+						changeToGreen ();
+						break;
+
+					case 1:
+						changeToYellow ();
+						break;
+
+					case 2:
+						changeToRed ();
+						break;
+
+					}
+
+					queuedLightTimes.RemoveAt (i);
+					queuedLightTypes.RemoveAt (i);
+
+				}
+
+			}
+
+			//demo ();
+
+		}
+
+
+		/// <summary>
+		/// Changes to green after the delay is up
+		/// </summary>
+		/// <param name="delay">Delay in seconds before it changes.</param>
+		public void changeToGreen(float delay){
+			queuedLightTimes.Add (Time.time + Mathf.Abs(delay));
+			queuedLightTypes.Add (0);
+		}
+
+
+		/// <summary>
+		/// Changes to yellow after the delay is up
+		/// </summary>
+		/// <param name="delay">Delay in seconds before it changes.</param>
+		public void changeToYellow(float delay){
+			queuedLightTimes.Add (Time.time + Mathf.Abs(delay));
+			queuedLightTypes.Add (1);
+		}
+
+
+		/// <summary>
+		/// Changes to red after the delay is up
+		/// </summary>
+		/// <param name="delay">Delay in seconds before it changes</param>
+		public void changeToRed(float delay){
+			queuedLightTimes.Add (Time.time + Mathf.Abs(delay));
+			queuedLightTypes.Add (2);
+		}
+
+		/// <summary>
+		/// Changes to green immediately
+		/// </summary>
+		public void changeToGreen(){
+			setOffset (0.667f);
+			transform.FindChild ("Lights").FindChild ("greenlight").GetComponent<Light> ().enabled = true;
+			transform.FindChild ("Lights").FindChild ("yellowlight").GetComponent<Light> ().enabled = false;
+			transform.FindChild ("Lights").FindChild ("redlight").GetComponent<Light> ().enabled = false;
+		}
+
+
+		/// <summary>
+		/// Changes to yellow immediately
+		/// </summary>
+		public void changeToYellow(){
+			setOffset (0.333f);
+			transform.FindChild ("Lights").FindChild ("greenlight").GetComponent<Light> ().enabled = false;
+			transform.FindChild ("Lights").FindChild ("yellowlight").GetComponent<Light> ().enabled = true;
+			transform.FindChild ("Lights").FindChild ("redlight").GetComponent<Light> ().enabled = false;
+		}
+
+
+		/// <summary>
+		/// Changes to red immediately
+		/// </summary>
+		public void changeToRed(){
+			setOffset (1f);
+			transform.FindChild ("Lights").FindChild ("greenlight").GetComponent<Light> ().enabled = false;
+			transform.FindChild ("Lights").FindChild ("yellowlight").GetComponent<Light> ().enabled = false;
+			transform.FindChild ("Lights").FindChild ("redlight").GetComponent<Light> ().enabled = true;
+		}
+
+
+		private void setOffset(float offset){
+			transform.FindChild ("Lights").GetComponent<MeshRenderer> ().material.mainTextureOffset = new Vector2(offset, 0);
+		}
+
+
+		private void demo(){
+			switch((int)(Time.time%3f)){
+
+			case 0:
+				changeToGreen (.2f);
+				break;
+
+			case 1:
+				changeToYellow (.5f);
+				break;
+
+			case 2:
+				changeToRed (.7f);
+				break;
+
+			}
+		}
+
+	}
+
+}
